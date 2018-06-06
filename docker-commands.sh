1bc9b9d8d9008706babbee3053c1b54a9dc75828
Phillip Anderson <phillip.anderson@salesforce.com>
2018-06-06 10:03:27 -0400

    Looks like Env Var will take care of password
    Still getting Error about API Key being set. Might also be able to remove HEROKU_EMAIL.

--- docker-commands.sh
+++ docker-commands.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+echo "Ready to build"
 docker build ./main -t philasfwork/sfmc-swagger-ui
+echo "image Built"
 docker images
+echo "Trying login"
 heroku container:login
+echo "logged in; trying web push"
 heroku container:push web -a sfmc-travis-tests
+echo "Image pushed."
