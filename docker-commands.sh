d40e43b6e930d3791994221be4adf3825b9032a9
Phillip Anderson <phillip.anderson@salesforce.com>
2018-06-06 11:28:39 -0400

    Heroku isn't giving out worker threads?
    Heroku uses something called Dyno's when calling the run command. It looks like either none are available, or they aren't there at all since this is the first deploy.

--- docker-commands.sh
+++ docker-commands.sh
@@ -2,10 +2,14 @@
 
 echo "Ready to build"
 docker build ./main -t philasfwork/sfmc-swagger-ui
+
 echo "image Built"
 docker images
+
 echo "Trying login"
 heroku container:login
+
 echo "logged in; trying web push"
 heroku container:push web -a sfmc-travis-tests
+
 echo "Image pushed."
